c68ee676b9f71fb05aff1b0318d42c0b3f7e0ea3
Jide A'. <45945474+jideabdqudus@users.noreply.github.com>
2026-02-10 23:31:21 +0000

    Time: 17 ms (81.12%), Space: 10.7 MB (83.4%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,10 +1,23 @@
+import "slices"
+
 func containsDuplicate(nums []int) bool {
-    seen := make(map[int]struct{})
-    for _, num := range nums {
-        if _, exists := seen[num]; exists {
+    /*
+        Brute Force Way:
+            - Sort the array nums
+            - Then have a ref number
+            - Create a for loop, if a is equals to ref, then return true
+            - If we get to the end of the loop without returning true, then return false
+            - If let of array is less than or equals to 1 return false
+    */
+    if len(nums) <= 1 {
+        return false
+    }
+
+    slices.Sort(nums)
+    for i := 1; i < len(nums); i++ {
+        if nums[i] == nums[i-1] {
             return true
         }
-        seen[num] = struct{}{}
     }
 
     return false
